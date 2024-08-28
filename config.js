4d9a14b950eeafb46e08ea54354152086a514e3b
Creative <115936592+vngctcreative@users.noreply.github.com>
2024-08-28 15:41:48 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bot_token: "", // Token Bot bỏ vào đây
+  ownerID: ["711115131064549498", ""], // Chủ sở hữu
+  commandsDir: './commands', // Lệnh gọi bot, thường là slash /
+  embedColor: "#1db954", // Màu tin nhắn embed
+  musicardTheme: "themes16", // Có thể tùy biến từ themes1 đến themes19
+  errorLog: "1246079347090264147", // Kênh báo lỗi tới server hỗ trợ BOT, không nên xóa
+  versionbot: "1.2", // Thêm phiên bản bot
+  footerEmbed: "Code By Creative", // Thêm footer cho embed
+  footerIcon: "" // Thêm icon cho footer, để trống nếu không sử dụng
+}
